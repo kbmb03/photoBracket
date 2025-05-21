c74eceb63f2383bc69790c5e18eb6c2c2897f12d
Kaleb Davis <kalebdavis03@gmail.com>
2025-05-21 12:04:52 -0700

    pick photos on new bracket button

--- photoBracket/ContentView.swift
+++ photoBracket/ContentView.swift
@@ -6,19 +6,43 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @StateObject private var pickerVM = PhotoPickerViewModel()
+    @State private var showPhotoPicker = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Photo Bracket!")
+                .font(.largeTitle)
+                .bold()
+                .padding(.top, 100)
+            Button(action: {
+                showPhotoPicker = true
+                print("starting new bracket")
+            }) {
+                Text("Start New Bracket")
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+                    .padding(.horizontal, 40)
+            }
+            Spacer()
         }
-        .padding()
+        .photosPicker(isPresented: $showPhotoPicker,
+                      selection: $pickerVM.selectedItems,
+                      matching: .images)
+        .onChange(of: pickerVM.selectedItems) { oldItems, newItems in
+            pickerVM.loadImages(from: newItems)
+        }
+                
     }
 }
-
-#Preview {
-    ContentView()
-}
+//
+//#Preview {
+//    ContentView()
+//}
