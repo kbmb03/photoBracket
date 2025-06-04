0174be235f2e0661161aa68d90d68459c950ca72
Kaleb Davis <kalebdavis03@gmail.com>
2025-06-03 17:14:13 -0700

    create photoItem struct to later store assetID

---
+++ photoBracket/photoObject.swift
@@ -0,0 +1,14 @@
+//
+//  photoObject.swift
+//  photoBracket
+//
+//  Created by Kaleb Davis on 6/3/25.
+//
+
+import Foundation
+import UIKit
+
+struct PhotoItem {
+    let image: UIImage
+    let assetID: String?
+}
